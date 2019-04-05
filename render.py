0c453ec00a2c1c910f050a9d0f55f05a3d1f4b3a
jens-colourbox <jens@colourbox.com>
2019-04-05 12:52:23 +0200

    Changed regex to allow for whitespace before brackets

--- render.py
+++ render.py
@@ -6,7 +6,7 @@ import re
 
 c = "".join(fileinput.input())
 
-r = boto3.client('ssm').get_parameters(Names=re.findall('{{ ?(.*) ?}}', c), WithDecryption=True)
+r = boto3.client('ssm').get_parameters(Names=re.findall('{{ ?([^ ]*) ?}}', c), WithDecryption=True)
 for p in r['Parameters']:
     c = c.replace('{{' + p['Name'] + '}}', p['Value'])
 
