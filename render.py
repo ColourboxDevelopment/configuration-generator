13c992d27cd05816b79c0b987d65673e33907b2d
jens-colourbox <jens@colourbox.com>
2019-04-10 10:35:24 +0200

    Ask for forgiveness when passing invalid params to boto
    If a config file contains no parameters our regex will produce no parameter names and boto will raise an exception.

--- render.py
+++ render.py
@@ -3,10 +3,15 @@ import boto3
 import sys
 import fileinput
 import re
+from botocore.exceptions import ParamValidationError
 
 c = "".join(fileinput.input())
 
-r = boto3.client('ssm').get_parameters(Names=re.findall('{{ ?([^ ]*) ?}}', c), WithDecryption=True)
+try:
+    r = boto3.client('ssm').get_parameters(Names=re.findall('{{ ?([^ ]*) ?}}', c), WithDecryption=True)
+except ParamValidationError:
+    r = {'Parameters': []}
+
 for p in r['Parameters']:
     c = re.sub('{{ ?' + p['Name'] + ' ?}}', p['Value'], c)
 
