ef4af99b9326e016a4d84fe3d5a690e877cc9288
Jens Kragelund Christensen <jens@colourbox.com>
2019-05-14 10:54:24 +0200

    Fail if output contains template string
    If a typo is introduced or a parameter is accidently emoved from ssm builds should fail.

--- render.py
+++ render.py
@@ -2,14 +2,22 @@
 import boto3
 import fileinput
 import re
+import sys
 
+pattern = '{{ ?([^ ]*) ?}}'
 c = "".join(fileinput.input())
 
-names = re.findall('{{ ?([^ ]*) ?}}', c)
+names = re.findall(pattern, c)
 if len(names) > 0:
-    r = boto3.client('ssm').get_parameters(Names=re.findall('{{ ?([^ ]*) ?}}', c), WithDecryption=True)
+    r = boto3.client('ssm').get_parameters(Names=re.findall(pattern, c), WithDecryption=True)
 
     for p in r['Parameters']:
         c = re.sub('{{ ?' + p['Name'] + ' ?}}', p['Value'], c)
+
+names = re.findall(pattern, c)
+if len(names) > 0:
+    for name in names:
+        sys.stderr.write("\nERROR: Configuration parameter was not replaced: \n\n\t{}\n\n".format(name))
+    sys.exit(1)
 
 print(c)
